e11c123db07ba7c76b0ca733e081d987e47835df
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-04-13 18:09:22 -0400

    added a print statement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.registrationform {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.registrationform to javafx.fxml;
--- src/main/java/org/example/registrationform/registrationController.java
+++ src/main/java/org/example/registrationform/registrationController.java
@@ -2,10 +2,13 @@ package org.example.registrationform;
 
 import javafx.fxml.FXML;
 
+import java.sql.SQLOutput;
+
 public class registrationController {
 
     @FXML
     protected void onHelloButtonClick() {
+        System.out.println("hello wordl, this is in the remote!");
         
     }
 }
