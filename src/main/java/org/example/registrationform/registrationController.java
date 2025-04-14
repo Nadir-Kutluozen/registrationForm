2c89aab904981ab8e931aed34d50810b809024a1
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-04-14 13:12:40 -0400

    added pattern, working on add btn

--- src/main/java/org/example/registrationform/registrationController.java
+++ src/main/java/org/example/registrationform/registrationController.java
@@ -15,6 +15,20 @@ public class registrationController {
     /* the add button responsible for all the fields validation*/
     private Button addBtn;
 
+    /* Regex patterns for validations*/
+
+    //min 2 max 25 {2,25}, this makes sure we have every character [A-Za-z]
+    private final String nameRegex = "^([A-Za-z]{2,25})\\s+([A-Za-z]{2,25})$";
+
+    // MM/DD/YYYY format.
+    private final String dobRegex = "^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\\d{4}$";
+
+    // +@farmingdale\.edu this makes sure that the user will enter the following "@farmingdale.edu".
+    private final String emailRegex = "^[A-Za-z0-9+_.-]+@farmingdale\\.edu$";
+
+    // 5-digit Zip Code.
+    private final String zipRegex = "^\\d{5}$";
+
     //todo
     //First Name and Last Name: Minimum of 2 characters and maximum of 25 characters.
     //Date of Birth: Should be in MM/DD/YYYY format.
@@ -25,6 +39,13 @@ public class registrationController {
     //Enable the "Add" button only if all fields contain valid data.
     //Otherwise it should remain disabled (grayed out).
     //if every entry is valid then, enable.
+
+
+    @FXML
+    public void initialize() {
+        addBtn.setDisable(true); // first disabled, later if all true, enable!
+
+    }
 
     @FXML
     void add(ActionEvent event) {
